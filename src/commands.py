280c847ff4bacb8b22ce9a9c2fc2c81ac3a47311
Micah <micahjank@gmail.com>
2020-05-12 13:29:10 -0500

    created commands.py

---
+++ src/commands.py
@@ -0,0 +1,9 @@
+# this file stores and keeps track of the available commands the player can do
+
+commands = {
+    "n": "Go North",
+    "e": "Go East",
+    "s": "Go South",
+    "w": "Go West",
+    "q": "Quit"
+}
