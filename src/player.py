4505750cc58994e48255f234735bfd47616d0b8c
Micah <micahjank@gmail.com>
2020-05-12 11:47:28 -0500

    added player class into the player.py

--- src/player.py
+++ src/player.py
@@ -1,2 +1,14 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+class Player:
+    def __init__(self, name, current_room):
+        self.name = name
+        self.current_room = current_room
+    
+    def __str__(self):
+        return self.name
+
+player = Player("Sir Micah the brave", "Outside")
+
+print(player)
